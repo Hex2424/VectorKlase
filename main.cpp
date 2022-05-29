03171eb9124890990354cff5cdf0a29354134b59
Hex2424 <maildetectionteam@gmail.com>
2022-05-29 23:58:35 +0300

    Time comparison check

--- main.cpp
+++ main.cpp
@@ -5,26 +5,26 @@
 
 int main()
 {
-    
-    unsigned int sz = 100000000;  // 100000, 1000000, 10000000, 100000000
+      // 100000, 1000000, 10000000, 100000000
     std::vector<int> v1;
-    Timer timer;
 
-    for (int i = 1; i <= sz; ++i)
-        v1.push_back(i);
-    auto delay = timer.stop() / 1000;
+    for(int iz= 100000 ; iz < 1000000000; iz *= 10)
+    {
+            Timer timer;
+            for (int i = 1; i <= iz; ++i)
+                v1.push_back(i);
+            auto delay = timer.stop() / 1000;
 
-    printf("%Lf\n", delay);
-    // Baigti v1 užpildymo laiko matavimą
+            Timer timer2;
+            Vector<int> v2;
+            for (int i = 1; i <= iz; ++i)
+                v2.push_back(i);
 
-    // Pradėti v2 užpildymo laiko matavimą
-    Timer timer2;
-    Vector<int> v2;
-    for (int i = 1; i <= sz; ++i)
-        v2.push_back(i);
-    auto delay2 = timer2.stop() / 1000;
-    printf("%Lf\n", delay2);
-// Baigti v2 užpildymo laiko matavimą
+            auto delay2 = timer2.stop() / 1000;
+            printf("STD: %Lf mano: %Lf\n", delay, delay2);
+        // Baigti v2 užpildymo laiko matavimą
+    }
+
     // std::vector<int> v;
 
     // Vector<int> vec(2);
